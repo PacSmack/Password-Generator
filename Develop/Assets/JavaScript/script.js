568740fbf544172958b1be4c23c9264d2c26fbc6
Nathan da Silva <pacsmacck@gmail.com>
2021-04-02 17:09:53 -0700

    var change

--- Develop/Assets/JavaScript/script.js
+++ Develop/Assets/JavaScript/script.js
@@ -5,29 +5,32 @@ var confirmNumericCharacter;
 var confirmUpperCase;
 var confirmLowerCase;
 
-var confirmLength = parseInt(prompt("How many characters would you like your password to contain?"));
-// determine length
-while (isNaN(confirmLength) || confirmLength <= 8 || confirmLength >= 128) {
-  alert("Your password must be between 8-128 characters \nTry Again");
+
+function generatePassword() {
   var confirmLength = parseInt(prompt("How many characters would you like your password to contain?"));
+  // determine length
+  while (isNaN(confirmLength) || confirmLength <= 8 || confirmLength >= 128) {
+    alert("Your password must be between 8-128 characters \nTry Again");
+    var confirmLength = parseInt(prompt("How many characters would you like your password to contain?"));
+  }
+
+  alert(`Your password will have ${confirmLength} characters`);
+
+  // determine other parameters
+
+  var confirmSpecialCharacter = confirm("Click OK to confirm if you want to include special characters");
+  var confirmNumericCharacter = confirm("Click OK to confirm if you want to include numerals");
+  var confirmUpperCase = confirm("Click OK to confirm if you want to include upper case letters");
+  var confirmLowerCase = confirm("Click OK to confirm if you want to include lower case letters");
+
+  while (confirmLowerCase === false && confirmUpperCase === false && confirmNumericCharacter === false && confirmSpecialCharacter === false) {
+    alert("You must choose at least one parameter");
+    var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+    var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");
+    var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+    var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
+  }
 }
-alert(`Your password will have ${confirmLength} characters`);
-
-// determine other parameters
-
-var confirmSpecialCharacter = confirm("Click OK to confirm if you want to include special characters");
-var confirmNumericCharacter = confirm("Click OK to confirm if you want to include numerals");
-var confirmUpperCase = confirm("Click OK to confirm if you want to include upper case letters");
-var confirmLowerCase = confirm("Click OK to confirm if you want to include lower case letters");
-
-while (confirmLowerCase === false && confirmUpperCase === false && confirmNumericCharacter === false && confirmSpecialCharacter === false) {
-  alert("You must choose at least one parameter");
-  var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
-  var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");
-  var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
-  var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
-}
-
 
 var randomFunc = {
   lower: getRandomLower,
